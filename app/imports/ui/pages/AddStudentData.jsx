e6ca5e94d710372bea3721347bf7d1b0c071236e
Philip Johnson <johnson@hawaii.edu>
2019-07-16 13:29:04 -1000

    Almost working

--- app/imports/ui/pages/AddStudentData.jsx
+++ app/imports/ui/pages/AddStudentData.jsx
@@ -10,15 +10,20 @@ import swal from 'sweetalert';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2'; //eslint-disable-line
 import SimpleSchema from 'simpl-schema';
 
+const hobbieValues = ['Surfing', 'Running', 'Biking', 'Paddling'];
+const levelValues = ['Freshman', 'Sophomore', 'Junior', 'Senior'];
+const gpaValues = ['4.0+', '3.0-3.9', '2.0-2.9', '1.0-1.9'];
+const majorValues = ['Physics', 'Math', 'Chemistry', 'Computer Science'];
+
 const formSchema = new SimpleSchema({
   name: { label: 'Name', type: String },
   bio: { label: 'Biographical Statement', type: String, optional: true, defaultValue: '' },
-  hobbies: { label: 'Hobbies', type: Array, allowedValues: ['Surfing', 'Running', 'Biking', 'Paddling'] },
-  'hobbies.$': String,
-  level: { label: 'Level', type: String, allowedValues: ['Freshman', 'Sophomore', 'Junior', 'Senior'] },
-  gpa: { label: 'GPA', type: String, allowedValues: ['4.0+', '3.0-3.9', '2.0-2.9', '1.0-1.9'] },
-  majors: { label: 'Majors', type: Array, allowedValues: ['Physics', 'Math', 'Chemistry', 'Computer Science'] },
-  'majors.$': String,
+  hobbies: { label: 'Hobbies', type: Array },
+  'hobbies.$': { type: String, allowedValues: hobbieValues },
+  level: { label: 'Level', type: String, allowedValues: levelValues, defaultValue: levelValues[0] },
+  gpa: { label: 'GPA', type: String, allowedValues: gpaValues },
+  majors: { label: 'Major(s)', type: Array, minCount: 1 },
+  'majors.$': { type: String, allowedValues: majorValues },
   enrolled: { label: 'Date Enrolled', type: Date, defaultValue: new Date() },
 });
 
@@ -26,8 +31,8 @@ const formSchema = new SimpleSchema({
 class AddStudentData extends React.Component {
 
   /** On submit, insert the data. */
-  submit() {
-    swal('Success', 'Item added successfully', 'success');
+  submit(data) {
+    swal('Success', `Data: ${JSON.stringify(data)}`, 'success');
   }
 
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
@@ -42,8 +47,8 @@ class AddStudentData extends React.Component {
                 <TextField name='name' showInlineError={true}/>
                 <LongTextField name='bio' showInlineError={true}/>
                 <SelectField name='hobbies' showInlineError={true}/>
-                <SelectField name='level' showInlineError={true}/>
-                <SelectField name='gpa' showInlineError={true}/>
+                <SelectField name='level' showInlineError={true} />
+                <SelectField name='gpa' showInlineError={true} placeholder={'Select one'} />
                 <SelectField name='majors' showInlineError={true}/>
                 <DateField name='enrolled' showInlineError={true}/>
                 <SubmitField value='Submit'/>

469e4b970a3c572915cfee2ee0fc63b7d2f46a88
Philip Johnson <johnson@hawaii.edu>
2019-07-17 08:36:49 -1000

    improve schema

--- app/imports/api/studentdata/studentdata.js
+++ app/imports/api/studentdata/studentdata.js
@@ -1,46 +1,26 @@
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
-import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2'; //eslint-disable-line
 
 /** Define a Mongo collection to hold student data. */
 const StudentData = new Mongo.Collection('StudentData');
 
+const StudentDataValues = {
+  hobbies: ['Surfing', 'Running', 'Biking', 'Paddling'],
+  level: ['Freshman', 'Sophomore', 'Junior', 'Senior'],
+  majors: ['Physics', 'Math', 'Chemistry', 'Computer Science'],
+};
+
 /** Define a schema to constrain the data and fields in the StudentData collection. */
 const StudentDataSchema = new SimpleSchema({
-  name: {
-    label: 'Name',
-    type: String,
-  },
-  bio: {
-    label: 'Biographical Statement',
-    type: String,
-    optional: true,
-    defaultValue: '',
-  },
-  hobbies: {
-    label: 'Hobbies',
-    type: Array,
-    allowedValues: ['Surfing', 'Running', 'Biking', 'Paddling'],
-  },
-  'hobbies.$': String,
-  level: {
-    label: 'Level',
-    type: String,
-    allowedValues: ['Freshman', 'Sophomore', 'Junior', 'Senior'],
-  },
-  gpa: {
-    label: 'GPA',
-    type: String,
-    allowedValues: ['4.0+', '3.0-3.9', '2.0-2.9', '1.0-1.9'],
-
-  },
-  majors: {
-    label: 'Majors',
-    type: Array,
-    allowedValues: ['Physics', 'Math', 'Chemistry', 'Computer Science'],
-  },
-  'majors.$': String,
+  name: String,
+  bio: { type: String, optional: true, defaultValue: '' },
+  hobbies: Array,
+  'hobbies.$': { type: String, allowedValues: StudentDataValues.hobbies },
+  level: { type: String, allowedValues: StudentDataValues.level },
+  gpa: Number,
+  majors: Array,
+  'majors.$': { type: String, allowedValues: StudentDataValues.majors },
 }, { tracker: Tracker });
 
 
@@ -48,4 +28,4 @@ const StudentDataSchema = new SimpleSchema({
 StudentData.attachSchema(StudentDataSchema);
 
 /** Make these objects available to others. */
-export { StudentData, StudentDataSchema };
+export { StudentDataValues, StudentData, StudentDataSchema };
